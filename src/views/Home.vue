0dd1fb9f15862a1bf96ce716f5dd0acac9c5e1f0
wuzhe-redmi <wuzhe_@aliyun.com>
2021-06-15 00:03:30 +0800

    初识BEM、ITCSS和ACSS

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <header>css架构</header>
+    <a href="http://www.baidu.com">百度</a>
+    <input type="text" value="heihei">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style lang="scss" scoped>
+header {
+  width: 200px;
+  height: 200px;
+  background: red;
+  color: #fff;
+  @include box-center(center, center);
+}
+  
+</style>
